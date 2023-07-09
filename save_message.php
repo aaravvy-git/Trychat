1c625aa02e431b95f28d96b93cc1e7b9ede456a4
saksham1059 <89568394+saksham1059@users.noreply.github.com>
2023-07-09 10:37:05 +0530

    Delete save_message.php

--- save_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $username = $_POST['username'];
-  $message = $_POST['message'];
-
-  // Append the username and message to the text file
-  $file = fopen("messages.txt", "a");
-  fwrite($file, $username . ": " . $message . PHP_EOL);
-  fclose($file);
-?>
